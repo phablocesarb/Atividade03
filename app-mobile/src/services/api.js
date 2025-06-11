1909f59d393baf587bcbc9cc02170c3665ddc1bc
phablocesarb <phablocesar@gmail.com>
2025-06-10 21:14:35 -0300

    Alterar o api.js e application.properties

--- app-mobile/src/services/api.js
+++ app-mobile/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 import { Platform } from "react-native";
 
-const LOCAL_IP = "192.168.100.5";
+const LOCAL_IP = "192.168.100.3";
 
 const baseURL =
   Platform.OS === "android"
